5109787bf19dcd76047899bcedb7d3773f5b07ce
alexveselun <alexveselun@gmail.com>
2024-12-19 03:04:23 +0200

    feat(text): fixed footer, fixed font size

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,12 +6,12 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="footer_container">
-        <p className="footer_text">© {new Date().getFullYear()} Всі права захищені.</p>
         <ul className="footer_links">
           <li>
-            <a href="mailto:felixredka@ukr.net" className="footer_link">Написати мені</a>
+            <a href="mailto:felixredka@ukr.net" className="footer_link">Організація концертів</a>
           </li>
         </ul>
+        <p className="footer_text">© {new Date().getFullYear()} Всі права захищені.</p>
       </div>
     </footer>
   );
